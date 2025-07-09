bfe90877a53ff058fa901a42a71297fb90dab873
shadid06 <hasnainbauet@gmail.com>
2025-07-09 17:49:28 +0600

    till sign

---
+++ 11.Math.cbrt.js
@@ -0,0 +1,10 @@
+// Math.cbrt() – Cube root
+
+// Math.cbrt() is used to find the cube root of a number.
+
+// Real life: What number cubed gives 27? Answer: 3
+
+const cubeRoot=Math.cbrt(27);
+console.log(cubeRoot); // 3
+
+// Project Use Case: 3D volume computations, advanced charts.
---
+++ 12.Math.sign.js
@@ -0,0 +1,13 @@
+// Math.sign() – Check if a number is positive or negative
+
+// Math.sign() method returns the sign of a number, indicating whether the number is positive, negative or zero.
+
+// If the number is positive, Math.sign() returns 1.
+// If the number is negative, Math.sign() returns -1.
+// If the number is zero, Math.sign() returns 0.
+
+console.log(Math.sign(5)); // 1
+console.log(Math.sign(-5)); // -1
+console.log(Math.sign(0)); // 0
+
+// Project Use Case: Detect upvote/downvote trends.
